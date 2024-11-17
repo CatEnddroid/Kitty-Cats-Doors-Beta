73254275251e352a891e0bcd5025b614fa8277b2
CatEnddroid <damon.nuber@gmx.ch>
2024-11-17 16:46:42 -0600

    Aktualisieren von hub.lua

--- hub.lua
+++ hub.lua
@@ -1,3 +1,5 @@
+--Thanks for using Kittys Cats Doors hub ui made by jake brocks
+
 local Fluent = loadstring(game:HttpGet("https://github.com/dawid-scripts/Fluent/releases/latest/download/main.lua"))()
 local SaveManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/SaveManager.lua"))()
 local InterfaceManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/InterfaceManager.lua"))()
